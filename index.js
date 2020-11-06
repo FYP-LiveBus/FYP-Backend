07d426fb5753a22d042507bf9247afd8885aaabd
Faran Ayyaz <faranayyaz@Faran.local>
2020-11-06 12:09:08 +0500

    CORS updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
-app.use(cors());
+// app.use(cors());
 app.use(bp.json());
 app.use(passport.initialize());
 
