6c22f9120a6800d2e45cb144cc0e4e635d126910
Myaccount3709 <75341969+Myaccount3709@users.noreply.github.com>
2020-12-12 03:29:43 +0500

    adding push notification

--- routes/Admin/notifications.js
+++ routes/Admin/notifications.js
@@ -1,6 +1,17 @@
 const { Notification} = require("../../models/Notification");
 const express = require("express");
 const router = express.Router();
+const admin = require("firebase-admin");
+const axios = require("axios");
+// const chunkArray = require("./chunkArray");
+
+var serviceAccount = require("../../config/servicsAccountKey.json");
+const { date } = require("joi");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://livebus-088091160.firebaseio.com"
+  });
 
 router.get("/", async (req, res) => {
   const notificatons = await Notification.find()
@@ -8,14 +19,85 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    
+
   let notificaton = new Notification({
     subject: req.body.subject,
     message: req.body.message,
   });
   notificaton = await notificaton.save();
-
+  await pushNotifications(req, res);
   res.send(notificaton);
 });
 
+async function pushNotifications(req, res) {
+    try {
+        const data = req.body;
+        
+        // Get users from Firestore, then build notifications array
+        await admin.firestore()
+        .collection("Tokens").get()
+        .then((querySnapshot) => {
+          if (querySnapshot.size) {
+            
+            // This array will contain each user’s notification
+            let notificationsArray = [];
+            
+            querySnapshot.forEach((doc) => {
+                let docData = doc.data();
+                if (docData && docData.token) {
+                    
+                    // The pushNotificationsToken retrieved from the app and stored in Firestore
+                    // if (docData.token) {
+                        notificationsArray.push({
+                            to: docData.token,
+                            title: data.subject,
+                            body: data.message,
+                        });
+                    // }
+                }
+            });
+                
+                // Send notifications to 100 users at a time (the maximum number that one Expo push request supports)
+                let notificationsChunks = chunkArray(notificationsArray, 100);
+                notificationsChunks.map((chunk) => {
+                    axios({
+                        method: "post",
+                        url: "https://exp.host/--/api/v2/push/send",
+                        data: chunk,
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+                });
+                return res.status(200).send({ message: "Notifications sent!" });
+            } else {
+                return res.status(404).send({ message: "No users found" });
+            }
+        })
+        .catch((error) => {
+            return res
+            .status(500)
+            .send({ message: `${error.code} - ${error.message}` });
+        });
+    } catch (error) {
+        return res
+        .status(500)
+        .send({ message: `${error.code} - ${error.message}` });
+    }
+}
+
+function chunkArray(myArray, chunk_size) {
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+  
+    for (index = 0; index < arrayLength; index += chunk_size) {
+      myChunk = myArray.slice(index, index + chunk_size);
+      tempArray.push(myChunk);
+    }
+  
+    return tempArray;
+}
+
 module.exports = router;
+
