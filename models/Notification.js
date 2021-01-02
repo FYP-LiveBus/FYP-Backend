42abe5a33fd3f48108e9d8b7e56b79a46545ac16
faranayyaz <faranayyaz@gmail.com>
2021-01-03 02:30:20 +0500

    Changes

--- models/Notification.js
+++ models/Notification.js
@@ -1,19 +1,21 @@
 // const Joi = require('joi');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Notification = mongoose.model('Notification', new mongoose.Schema({
-  subject: {
-    type: String,
-    required: true,
-  },
-  message:{
-    type: String,
-    required: true,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now
-  }
-}));
+const Notification = mongoose.model(
+  "Notification",
+  new mongoose.Schema({
+    subject: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+    },
+  })
+);
 
-exports.Notification = Notification; 
+exports.Notification = Notification;
