63d2494a9e3dca59398f17b4e6d20f3e82eb268d
faranayyaz <faranayyaz@gmail.com>
2021-01-10 23:30:37 +0500

    Changes

--- models/StudentTripsDetail.js
+++ models/StudentTripsDetail.js
@@ -1,25 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const StudentTripsDetail = mongoose.model('StudentTripsDetail', new mongoose.Schema({
-  stdUsername:{
-    type: String,
-    required: true,
-  },
-  email:{
-    type: String,
-    required: true
-  },
-  routeNo: {
-    type: Number,
-    required: true
-  },
-  stopName: {
-    type: String,
-    required: true,
-  },
-  date: { 
-    type: String,
-  }
-}));
+const StudentTripsDetail = mongoose.model(
+  "StudentTripsDetail",
+  new mongoose.Schema({
+    stdUsername: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    routeNo: {
+      type: Number,
+      required: true,
+    },
+    stopName: {
+      type: String,
+      required: true,
+    },
+    driver: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+    },
+  })
+);
 
-exports.StudentTripsDetail = StudentTripsDetail; 
+exports.StudentTripsDetail = StudentTripsDetail;
