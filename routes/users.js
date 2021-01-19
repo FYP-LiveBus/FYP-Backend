c86f786233d9a6bace42df149917bec8c2320915
faranayyaz <faranayyaz@gmail.com>
2021-01-20 04:59:05 +0500

    Changes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 // Bring in the User Registration function
 const {
   userAuth,
